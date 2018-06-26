32180ea39b9a98065551c829e9743370c2330106
kevin kristian <kevinkristianz@gmail.com>
2018-06-26 12:46:32 +0700

    update v.0.5: added dataset cropping + dataset generator

---
+++ dataset_generator/run.txt
@@ -0,0 +1,8 @@
+
+python module.py #generated tf record
+
+python module_2.py --image_number=80 --class_number=10 --image_height=30 --image_width=30 #read dataset tf record
+
+#requirement to run
+#tensorflow
+#python 3.6
--- module_3.py
+++ module_3.py
@@ -1,6 +1,6 @@
 # module 3 image enhancement
 #
-# deskripsi: untuk enhance image menjadi terbaca dan bagus
+# deskripsi: untuk enhance image menjadi terbaca dan bagus dan membuat dataset tiap angka
 # input : folder out hasil transformasi
 # output: folder out di C: untuk menyesuaikan tessract OS windows
 #
@@ -20,6 +20,49 @@ for img in images:
 
 	#img = cv2.imread("out/test.png",0)
 
+	#contouring
+	cnts = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
+	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+	locs = []
+	rec = img
+	counter2 = 0
+
+	for (i, c) in enumerate(cnts):
+		# compute the bounding box of the contour, then use the
+		# bounding box coordinates to derive the aspect ratio
+		(x, y, w, h) = cv2.boundingRect(c)
+		ar = w / float(h)
+	 	#print("masuk_loop")
+		# since credit cards used a fixed size fonts with 4 groups
+		# of 4 digits, we can prune potential contours based on the
+		# aspect ratio
+		#if ar > 2.5 and ar < 4.0:
+			# contours can further be pruned on minimum/maximum width
+			# and height
+		if (w > 10 and w < 290) and (h > 15 and h < 45):
+			# append the bounding box region of the digits group
+			# to our locations list
+			locs.append((x, y, w, h))
+			#rec = cv2.rectangle(img,(x,y),(w+x,h+y),(0,255,0),3)
+			#print("masuk_if")
+
+	#print(len(locs))
+
+	clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+
+	
+	for (x,y,w,h) in locs:
+		maxw = 16
+		maxh = 23
+		#print(`w`+" "+`h`)
+		counter2 = counter2 + 1
+		imgg = img[y:y+h,x:x+w]
+		equ = cv2.equalizeHist(imgg)
+		name2 = "dataset"+`counter2`+"_pic"+`counter`
+		cv2.imwrite("out/ktp/transform/dataset/"+name2+".png", equ)
+		cv2.waitKey(0)
+
 	equ = cv2.equalizeHist(img)
 	res = np.hstack((img,equ))
 
@@ -28,5 +71,5 @@ for img in images:
 	cv2.imwrite("C:/test/out/"+name+".png", equ)
 
 	#cv2.imwrite("C:/test/out/hasil.png", equ)
-	#cv2.imshow("th3", res)
+	#cv2.imshow("th3", img)
 	cv2.waitKey(0)
